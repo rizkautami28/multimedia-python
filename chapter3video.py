72b715557b2465520b4b0a7748de84fc58e0b752
Rizka17 <105841111421@student.unismuh.ac.id>
2024-08-17 14:28:02 +0800

    chapter3video.py

--- chapter3video.py
+++ chapter3video.py
@@ -1,6 +1,11 @@
 from moviepy.editor import VideoFileClip
 from moviepy.editor import concatenate_videoclips
 from moviepy.editor import vfx
+import tkinter as tk
+from PIL import Image, ImageTk
+from tkinter import filedialog
+from pydub import AudioSegment
+from pydub.playback import play
 
 video = VideoFileClip('myvideo.mp4')
 
@@ -17,3 +22,23 @@ sped_up_video = short_video.fx(vfx.speedx, 2)
 sped_up_video.write_videofile('SpeedUp_result.mp4')
 
 video.write_videofile('result.mp4')
+
+root = tk.Tk()
+root.title("Multimedia Application")
+
+image = Image.open('bunga.jpg')
+photo = ImageTk.PhotoImage(image)
+
+label = tk.Label(root, image=photo)
+label.pack()
+
+def play_music():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        audio = AudioSegment.from_file(file_path)
+        play(audio)
+
+play_button = tk.Button(root, text="Play", command=play_music)
+play_button.pack()
+
+root.mainloop()
